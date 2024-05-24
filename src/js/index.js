c2066f64c07fdaa531ea4a8757959dee06fad5f4
jdOJ <162026593+jdOJ@users.noreply.github.com>
2024-05-24 07:29:55 +0000

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,45 @@
-//import react into the bundle
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock } from '@fortawesome/free-solid-svg-icons';
 
-// include your styles into the webpack bundle
-import "../styles/index.css";
+function SecondCounter(props) {
+    return (
+        <div className="bigCounter d-flex justify-content-center bg-black text-white fs-1">
+            <div className="calendar p-3 m-3 border-black bg-dark"><FontAwesomeIcon icon={faClock} /></div>
+            <div className="four p-3 m-3 border-black bg-dark">{props.digitFourth}</div>
+            <div className="three p-3 m-3 border-black bg-dark">{props.digitThird}</div>
+            <div className="two p-3 m-3 border-black bg-dark">{props.digitSecond}</div>
+            <div className="one p-3 m-3 border-black bg-dark">{props.digitFirst}</div>
+        </div>
+    );
+}
 
-//import your own components
-import Home from "./component/home.jsx";
+SecondCounter.propTypes = {
+    digitFourth: PropTypes.number,
+    digitThird: PropTypes.number,
+    digitSecond: PropTypes.number,
+    digitFirst: PropTypes.number
+};
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let counter = 0;
+setInterval(function () {
+    const digitFourth = Math.floor(counter / 1000) % 10;
+    const digitThird = Math.floor(counter / 100) % 10;
+    const digitSecond = Math.floor(counter / 10) % 10;
+    const digitFirst = Math.floor(counter / 1) % 10;
+    counter++;
+    console.log(digitFourth, digitThird, digitSecond, digitFirst);
+    
+    ReactDOM.render(
+        <SecondCounter
+            digitFirst={digitFirst}
+            digitSecond={digitSecond}
+            digitThird={digitThird}
+            digitFourth={digitFourth}
+        />, document.querySelector("#app")
+    );
+}, 1000);
+
+export default SecondCounter;
